b5ac29cbb32eae800caccf1e2969ddb473227805
anilannamalair <anilannamalair@gmail.com>
2025-03-13 15:17:02 +0530

    Update deployProject.groovy

--- vars/deployProject.groovy
+++ vars/deployProject.groovy
@@ -1,3 +1,4 @@
-def call() {
-    echo "Deploying the Java project..."
-}
+// deploy_project.groovy
+println "Deploying project..."
+sh 'mvn deploy'  // Replace with your deploy logic (e.g., to a server or cloud).
+
