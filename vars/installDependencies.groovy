d3094f398a66efd7245b6da70504831e92098fbc
anilannamalair <anilannamalair@gmail.com>
2025-03-13 15:16:10 +0530

    Update installDependencies.groovy

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,3 +1,3 @@
-def call() {
-    echo "Installing dependencies with Maven..."
-}
+// install_dependencies.groovy
+println "Installing dependencies..."
+sh 'mvn install'  // Assuming you're using Maven, replace this with your dependency installation logic.
