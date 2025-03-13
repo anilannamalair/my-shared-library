d0ab4dd6b15e76f37dce528becabdff452ba8d17
anilannamalair <anilannamalair@gmail.com>
2025-03-13 15:16:31 +0530

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -1,3 +1,3 @@
-def call() {
-    echo "Building the Java project with Maven..."
-}
+// build_project.groovy
+println "Building project..."
+sh 'mvn clean install'  // Replace with your actual build logic (e.g., Maven, Gradle, etc.)
