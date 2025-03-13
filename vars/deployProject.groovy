edafc86c3ff239e9d819f204ac89061d7b433ba1
anilannamalair <anilannamalair@gmail.com>
2025-03-13 13:33:24 +0530

    Update deployProject.groovy

--- vars/deployProject.groovy
+++ vars/deployProject.groovy
@@ -1,5 +1,3 @@
 def call() {
-    echo "Deploying the Java project..."
-    // Here you could add logic to deploy the project, e.g., copying jars, uploading to a server, etc.
-    sh 'echo "Deploying to the production server..."'
+    echo "Deploying the Java project...
 }
